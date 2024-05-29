a5e7467967cbfeac82646d66f3bddce53ade0016
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-05-29 18:07:17 +0530

    first commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import '../styles/LoginPage.css';
+
+const LoginPage = ({ setRole }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/route53/login', { username, password });
+      if (response.data.message === 'Login successful') {
+        navigate('/'); 
+      }
+      else{
+        navigate('/domainlist')
+      }
+    } catch (error) {
+      setError('Invalid credentials');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
